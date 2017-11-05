ece464acf6b1b0971faac452164e5e2a2ec9c837
chandlr2 <31193446+chandlr2@users.noreply.github.com>
2017-11-05 18:28:29 -0500

    Update bridge_2.py

--- bridge_2.py
+++ bridge_2.py
@@ -24,13 +24,59 @@ if len(myargs.keys()) != 0:
 	sys.exit()
 # }
 
-
 credentials2 = pika.PlainCredentials(host, 'raspberry')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.29.124.160',credentials = credentials2 ))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='direct_logs',
                          exchange_type='direct')
+
+###################################################################3
+
+from bluetooth import *
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+                   
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+	while True:
+		data = client_sock.recv(1024)
+		data = str(data)
+		command = data[2] +data[3]
+		print(data)
+		print(command)
+		if command is 'p:':
+			print("cool")
+		if len(data) == 0: break
+		print("received [%s]" % data)
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
+print("all done")
+
+
+
 
 severity = sys.argv[1] if len(sys.argv) > 2 else 'info'
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
