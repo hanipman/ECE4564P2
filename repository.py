73c829624f9db3aea1fafaee312999407d72b6bc
Chris Boado <boadoct@vt.edu>
2017-11-05 14:31:17 -0500

    Add repository.py

---
+++ repository.py
@@ -0,0 +1,9 @@
+#Repository
+#Service that manages
+# - messages sent from phone
+# - message requests to a mobile phone
+#Processes RabbitMQ produce and consume operations
+#Indicates system status using terminal output
+
+import pika
+
