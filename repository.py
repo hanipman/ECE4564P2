4b2178313286e625534569cc50bb47af22a12233
hanipman <boadoct@vt.edu>
2017-11-13 17:58:05 +0000

    Updated Repo

--- repository.py
+++ repository.py
@@ -25,6 +25,7 @@ queue_name = result.method.queue
 channel.queue_purge(queue_name)
 channel.queue_unbind(queue=queue_name, exchange=rmq_params["exchange"], routing_key=queue_name)
 channel.queue_bind(exchange=rmq_params["exchange"], queue=queue_name, routing_key=queue_name)
+channel.queue_bind(exchange=rmq_params["exchange"], queue=rmq_params["master_queue"], routing_key=queue_name)
 
 #Creates a queue for every item in the queues set and binds it to the exchange
 #Also creates binds the master queue to every new routing key
@@ -43,14 +44,16 @@ while count < len(list_queue):
 def callback(ch, method, properties, body):
     print("[Checkpoint 03] Consumed a message published with routing_key: '" + method.routing_key + "'")
     print("[Checkpoint 04] Message: " + str(body))
-    if str(body) == 'red':
-        print("[Checkpoint 1-01] Flashing LED red")
-    elif str(body) == 'green':
-        print("[Checkpoint 1-01] Flashing LED green")
-    elif str(body) == 'blue':
+    if str(body) == 'b\'purple\'':
+        print("[Checkpoint 1-01] Flashing LED purple")
+    elif str(body) == 'b\'yellow\'':
+        print("[Checkpoint 1-01] Flashing LED yellow")
+    elif str(body) == 'b\'blue\'':
         print("[Checkpoint 1-01] Flashing LED blue")
-    elif str(body) == 'white':
-        print("[Checkpoint 1-01] Flashing LED white")
+    elif str(body) == 'b\'green\'':
+        print("[Checkpoint 1-01] Flashing LED green")
+    elif str(body) == 'b\'red\'':
+        print("[Checkpoint 1-01] Flashing LED red")
 
 print("[Checkpoint 02] Consuming messages from '" + rmq_params["master_queue"] + "' queue")
 channel.basic_consume(callback,
